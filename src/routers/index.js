922da3742c3aaea096f3582147ae2bf3d90f12f7
dv-kong <70198643+dv-kong@users.noreply.github.com>
2021-03-30 22:29:21 +0200

    Update db.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -8,7 +8,6 @@ const tweetController = require("../controllers/tweetController");
 const router = express.Router();
 
 // CREATE, POST, GET ONE OR GET ALL TWEETS
-<<<<<<< HEAD
 // router.get("/", tweetController.findAllTweets);
 // router.get("/tweets/:id", isAuth, tweetController.findOneTweet);
 router.post("/tweets", tweetController.addOneTweet);
@@ -26,16 +25,12 @@ router.post("/tweets/edit/:id", tweetController.editOneTweet);
 //   });
 
 
-=======
-// router.get("/tweet:id", tweetController.getAllTweets);
-router.get("/", tweetController.findAllTweets);
->>>>>>> wip
 // INSCRIPTION 
 // router.get("/signup/:id", userController.signUp); // recupérer la page d'inscriptions
 // router.post("/signup", userController.newAccount); // recupère   les donées de l'utilisateur puis le redirige vers la page login
 
 // AUTHENTICATION
-router.get("/login", userController.logIn); 
+router.get("/login", userController.logIn);
 router.post("/login", userController.authentificate);
 
 // router.get("/logout", userController.logOut);
