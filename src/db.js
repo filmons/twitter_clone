552ea02b7229d719084812c385dbd5a4f1bf4a6d
dv-kong <70198643+dv-kong@users.noreply.github.com>
2021-03-30 22:29:21 +0200

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,8 +3,13 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
+<<<<<<< HEAD
     password: "0123abcdefG.", // input database password
     database: "tweet_clone"
+=======
+    password: "", // input database password
+    database: "twitter_clone"
+>>>>>>> Update db.js
 });
 
 module.exports = db;
