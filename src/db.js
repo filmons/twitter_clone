e42841fbb5d2bd4e3a1866d1960e1b178b2e2a55
seare <73501892+filmons@users.noreply.github.com>
2021-03-31 15:35:28 +0200

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "", // input database password
+    password: "root", // input database password
     database: "twitter_clone"
 });
 
