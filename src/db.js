70a6df2e3b5e08f24c9ea6e8ee8c15f6f4bef2a5
dv-kong <zektbach@outlook.com>
2021-04-01 16:38:55 +0200

    local changes

--- src/db.js
+++ src/db.js
@@ -4,10 +4,14 @@ const db = mysql.createConnection({
     host: "localhost",
     user: "root",
 <<<<<<< HEAD
+<<<<<<< HEAD
     password: "0123abcdefG.", // input database password
     database: "tweet_clone"
 =======
     password: "", // input database password
+=======
+    password: "!1myCetvu6hh", // input database password
+>>>>>>> local changes
     database: "twitter_clone"
 >>>>>>> Update db.js
 });
