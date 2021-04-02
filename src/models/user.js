729584866bf4ca786ffd0d615d72a14aef1749a0
dv-kong <zektbach@outlook.com>
2021-04-02 22:10:21 +0200

    signup, signin merged with authentification and logout

--- src/models/user.js
+++ src/models/user.js
@@ -4,10 +4,9 @@ const db = require("../db");
 exports.getByUsername = (username, callback) => { // callback = function
     console.log(`got into user.js/getByUsername` + ` username value: ` + username);
     // console.log(`username value: ` + username);
-
     // console.log(`getByUsername(username) value: ` + username);
     // db.query(`SELECT username FROM user WHERE username = "${username}";`), (error, result) => {
-    db.query(`SELECT * FROM user WHERE username = "${username}";`, (error, result) => {
+    db.query(`SELECT * FROM user WHERE username = "${username}";`, (error, result) => { // if get result, > callback
             if (error) {
                 console.log("Error:", error);
                 callback(error, null);
@@ -20,58 +19,33 @@ exports.getByUsername = (username, callback) => { // callback = function
         // check every step 
 }
 
-exports.createUser = (user, callback) => {
-    // const user = requestBodyObject;  
-    console.log(`createUser: ` + user);
-    // console.log(`+++(user): ${user}`); // need obj that contains obj.name pass etc
-    // console.log(`+++(user) keys: ` + Object.keys(user));
-    // console.log(`+++(params) : ${requestBodyObject}`);
-    // db.query(`INSERT INTO user (name, username, password) VALUES ("${user.name}", "${user.username}", "${user.password}");`, (error, result) => {
-    // db.query(`INSERT INTO user (username, password, first_name, last_name, birth_date, email, phone_number, city) 
-    //   VALUES ("${user.username}", "${user.password}", "${user.first_name}", "${user.last_name}", "${user.birthday}",
-    //     "${user.email}", "${user.phone_number}", "${user.city}");`, (error, result) => {
-    db.query(`INSERT INTO user 
-    (username, password, first_name, last_name, 
-        birth_date, email, phone_number, city) 
-      VALUES ("${user.username}", "${user.password}", 
-      "${user.first_name}", "${user.last_name}", "${user.birth_day}",
-        "${user.email}", "${user.phone_number}", 
-        "${user.city}");`, (error, result) => {
-
-
-<<<<<<< HEAD
-
-        // db.query(`SELECT * FROM user;`, (error, result) => {
-
-        if (error) {
-            console.log("error: ", error);
-            callback(error, null);
-            return;
-        }
-        callback(null, result);
-    })
-=======
-exports.getByUsername= (username, callback) => {
-  db.query(`SELECT * FROM user WHERE username = "${username}";`, (error, result) => {
-    if (error) {
-      console.log("error: ", error);
-      callback(error, null);
-      return;
-    }
-    callback(null, result);
-    }) 
->>>>>>> logout and  authentification OK, remaining set hash feature
-}
-
-// AFTER THAT I CREATE A NEW USER'S
-
-exports.authentificate = (username, callback) => {
+exports.usernameCheck = (username, callback) => {
     db.query(`SELECT * FROM user WHERE username = "${username}";`, (error, result) => {
         if (error) {
             console.log("error: ", error);
             callback(error, null);
             return;
         }
+        callback(null, result); // leads to /userController.js/User.usernameCheck
+    })
+}
+
+exports.createUser = (user, callback) => {
+    console.log(`createUser: ` + user);
+    // console.log(`+++(user): ${user}`); // need obj that contains obj.name pass etc
+    console.log(`+++(user) keys: ` + Object.keys(user));
+    db.query(`INSERT INTO user 
+    (username, password, first_name, last_name, 
+        birth_date, email, phone_number, city) 
+      VALUES ("${user.username}", "${user.password}", 
+      "${user.first_name}", "${user.last_name}", "${user.birth_day}",
+        "${user.email}", "${user.phone_number}", "${user.city}");`, (error, result) => {
+        if (error) {
+            console.log("error: ", error);
+            callback(error, null);
+            return;
+        }
+
         callback(null, result);
     })
 }
