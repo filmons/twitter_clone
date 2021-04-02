24e469dc09486032f5f0a723a82262781bf2d4ab
dv-kong <zektbach@outlook.com>
2021-04-02 21:25:33 +0200

    cleanup

--- src/routers/index.js
+++ src/routers/index.js
@@ -37,14 +37,3 @@ router.post("/login", userController.authentificate);
 router.get("/logout", userController.logOut);
 
 module.exports = router;
-
-// SANDRO EXEMPLE ROUTER 
-
-// router.get("/promos/:id", isAuth, promoController.findOne);
-// router.post("/promos", promoController.addOne);
-
-// router.get("/signup", userController.signup);
-// router.post("/signup", userController.newAccount);
-// router.get("/login", userController.login);
-// router.post("/login", userController.authenticate);
-// router.get("/logout", userController.logout);
